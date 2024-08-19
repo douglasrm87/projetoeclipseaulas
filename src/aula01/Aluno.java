72b402a0da5a50f06a7db82fc1c551e866b973ce
douglas <douglas.mendes@copel.com>
2024-08-19 19:53:27 -0300

    aula 01

---
+++ src/aula01/Aluno.java
@@ -0,0 +1,22 @@
+package aula01;
+
+// Nome da classe. Também no do programa.
+public class Aluno {
+	private String nome;
+	public void inserirNome(String n) {
+		// código fonte para gravar o nome no BD.
+		this.nome = n;
+	}
+	public String recuperarNome() {
+		// código fonte para ler o nome no BD.
+		return this.nome;
+	}
+	// Tela gráfica simulando a interface do usuário.
+	public static void main(String[] args) {
+		Aluno obj1 = new Aluno();
+		obj1.inserirNome("Douglas");
+		System.out.println("Saida: "+obj1.recuperarNome());
+	}
+	
+
+}
--- src/aula01/Teste.java
+++ src/aula01/Teste.java
@@ -1,10 +1,6 @@
 package aula01;
-
 public class Teste {
-
 	public static void main(String[] args) {
 		System.out.println("Teste");
-
 	}
-
 }
